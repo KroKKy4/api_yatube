67cafa98e0812fb6b8c2e4705baf6c41317c2f60
Ус Максим <usmaxim@mail.ru>
2024-04-06 13:08:26 +0300

    Second

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -30,7 +30,7 @@ class CommentViewSet(GenericViewSet, ListModelMixin, CreateModelMixin):
 
     def get_queryset(self):
         post = get_object_or_404(Post, pk=self.kwargs.get('post_id'))
-        return post.comments.all()
+        return post.comments
 
     def perform_create(self, serializer):
         post = get_object_or_404(Post, pk=self.kwargs.get('post_id'))
