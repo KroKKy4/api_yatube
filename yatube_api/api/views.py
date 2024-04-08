9caf0cbaa88cf29ad047e1374844747a59e51b60
Ус Максим <usmaxim@mail.ru>
2024-04-08 13:31:50 +0300

    Second(2)

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -2,8 +2,6 @@ from django.shortcuts import get_object_or_404
 from posts.models import Group, Post
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.generics import ListCreateAPIView
-from rest_framework.mixins import CreateModelMixin
 
 from .permissions import IsOwnerOrReadOnly
 from .serializers import CommentSerializer, GroupSerializer, PostSerializer
@@ -22,8 +20,17 @@ class GroupViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
 
+'''
+Дмитрий, здравствуйте!
+Написал вам в пачке по поводу данного задания, но ответа так и не получил.
+Хотел бы уточнить информацию по поводу исправления кода, по заданию, ендпоинт
+api/v1/posts/{post_id}/comments/{comment_id}/ должен обладать следующими
+методами: GET, PUT, PATCH, DELETE. Можете, пожалуйста, уточнить зачем
+реализовывать класс через генерики и миксины, если нам нужны все методы?
+'''
 
-class CommentViewSet(ListCreateAPIView, CreateModelMixin):
+
+class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
     permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
 
